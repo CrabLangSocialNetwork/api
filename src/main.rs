f3ade8c308ef980e2c43373c9908d59d4e9f7712
RIC217 <melericcardo9400@gmail.com>
2023-06-06 18:09:08 +0200

    Added simple base code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use serde::{Serialize, Deserialize};
+use surrealdb::{Surreal, engine::remote::ws::Ws, opt::auth::Root, sql::Thing};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct User {
+    id: Option<Thing>,
+    username: String,
+    password: String,
+    email: String
+}
+
+#[tokio::main]
+async fn main() -> surrealdb::Result<()> {
+    let db = Surreal::new::<Ws>("localhost:8000").await?;
+    
+    db.signin(Root {
+        username: "root",
+        password: "root"
+    }).await?;
+
+    db.use_ns("test").use_db("test").await?;
+
+    let created: User = db.create("user")
+        .content(User {
+            id: None,
+            username: "username".to_string(),
+            password: "password".to_string(),
+            email: "test@example.com".to_string()
+        })
+        .await?;
+    dbg!(created);
+
+    Ok(())
 }
